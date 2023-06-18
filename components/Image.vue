eef186b5dd1247d6d32e7a03a359be32eecce9b5
FarruxDEV <farruxbeknozimov@gmail.com>
2023-06-18 17:08:24 +0500

    add projects

--- components/Image.vue
+++ components/Image.vue
@@ -1,14 +1,34 @@
 <template>
-	<div class="relative">
-		<img class="object-cover cursor-pointer w-full h-full" :src="src" />
-		<IconCSS
-			name="gridicons:fullscreen"
-			class="absolute text-white text-2xl top-0 right-0 bg-black" />
+	<div>
+		<div
+			class="absolute bg-black/50 z-50 top-0 left-0 flex items-center justify-center duration-300"
+			:class="full ? 'w-full h-full' : 'w-0 h-0'">
+			<img
+				class="object-cover rounded-xl cursor-pointer w-[80%] h-[80%]"
+				:src="src" />
+			<IconCSS
+				@click="() => (full = false)"
+				name="ph:x-bold"
+				class="cursor-pointer hover:scale-110 absolute text-white text-3xl top-5 right-5" />
+		</div>
+		<div class="relative h-full">
+			<img
+				class="object-cover cursor-pointer w-full h-full rounded-lg"
+				:src="src" />
+			<div
+				class="absolute top-1 right-1 bg-black/70 border border-cyan-500 rounded">
+				<IconCSS
+					@click="() => (full = true)"
+					name="gridicons:fullscreen"
+					class="cursor-pointer hover:scale-110 text-white text-2xl" />
+			</div>
+		</div>
 	</div>
 </template>
 
 <script setup>
 const { src } = defineProps(["src"]);
+const full = ref(false);
 </script>
 
 <style lang="scss" scoped></style>
--- pages/works/index.vue
+++ pages/works/index.vue
@@ -3,13 +3,22 @@
 <template>
 	<div>
 		<div class="w-full px-32 py-20">
-			<div class="grid grid-cols-3">
-				<div class="bg-slate-700/50 rounded-xl min-h-[400px] p-3">
+			<div class="grid grid-cols-3 gap-5">
+				<div
+					v-for="el in 5"
+					class="border border-cyan-500 bg-slate-700/50 rounded-lg min-h-[300px] p-3">
 					<Image
 						src="https://upcdn.io/FW25bPH/raw/uploads/2023/06/18/Screenshot%20from%202023-06-18%2015-20-53-3E5Y.png"
-						class="h-[200px] w-full object-cover rounded-xl" />
-					<h1 class="text-cyan-300 text-3xl text-center p-3">Course</h1>
-					<span class="text-cyan-200 bg-gray-500 p-2 p-1">#nodejs</span>
+						class="h-[150px] w-full object-cover rounded-xl" />
+					<h1 class="text-cyan-300 text-2xl text-center mt-3">Course</h1>
+					<p class="text-sm mb-5 text-cyan-500 text-center">
+						You can test your algorithm skills in javascript on this website
+					</p>
+					<span
+						v-for="el in 3"
+						class="cursor-pointer text-cyan-200 mr-2 bg-gray-950/50 hover:bg-gray-950 duration-300 p-1 rounded">
+						#nodejs
+					</span>
 				</div>
 			</div>
 		</div>
